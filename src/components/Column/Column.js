1783db0a4b1f995e3b8b72ed5885c10608c5dc2a
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2020-01-20 18:00:01 +0100

    Add [partly] Card component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-
+import Card from '../Card/Card';
 
 const Column = props => (
     <section className={styles.component}>
         <h3 className={styles.title}>{props.title}</h3>
+        <div>
+            <Card />
+        </div>
     </section>
 );
 
 Column.propTypes = {
-    columnTitle: PropTypes.node,
+    title: PropTypes.node.isRequired,
   };
 
 export default Column;
