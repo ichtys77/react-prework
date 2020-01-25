d58720a3aba1acb4908aad138f0bfeb9fd9bde21
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2020-01-25 11:25:42 +0100

    Add cards display in Column component with data from redux store

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-// import Card from '../Card/Card';
+import Card from '../Card/Card';
 import { settings } from '../../data/dataStore';
 // import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
@@ -19,18 +19,18 @@ class Column extends React.Component {
   }
 
   render() {
-    const {title, icon} = this.props;
+    const {title, icon, cards} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{title}
           <span className={styles.icon}><Icon name={icon} /></span>
         </h3>
-        {/*
         <div>
-          {this.state.cards.map(({ key, ...cardsProps }) => (
-            <Card key={key} {...cardsProps} />
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
           ))}
         </div>
+        {/*
         <div>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
         </div>
