315babbd417b93d4100d4beffde5f03cd5fc9cb6
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2020-02-04 18:59:42 +0100

    Add Search component

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -10,13 +10,13 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const ADD_CARD = createActionName('ADD_CARD');
-export const MOVE_CARD = createActionName('MOVE_CARD');
+//export const MOVE_CARD = createActionName('MOVE_CARD');
 
 // action creators
 export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARD });
 //console.log({createActionAddCard});
 
-export const createAction_moveCard = payload => ({ payload, type: MOVE_CARD });
+//export const createAction_moveCard = payload => ({ payload, type: MOVE_CARD });
 //console.log({createAction_moveCard});
 
 // reducer
